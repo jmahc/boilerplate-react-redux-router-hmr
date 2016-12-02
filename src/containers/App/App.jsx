219e6f653f9fa7d6696c751a53341119fc106eae
Jordan McArdle <jordanmcardle@gmail.com>
2016-12-02 16:36:13 -0600

    ESLINT updates and other package corrections.  Working with HMR + React-Router.  Will implement Redux next :)

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-export default class App extends React.Component { // eslint-disable-line react/prefer-stateless-function, max-len
-  render() {
-    const { children } = this.props; // eslint-disable-line react/prop-types
-    return (
-      <div className="layout">
-        <div className="header">
-          <h1>HEADER!</h1>
-          <hr />
-        </div>
-        <div className="content">
-          {children}
-        </div>
-        <div className="footer">
-          <hr />
-          <h1>FOOTER</h1>
-        </div>
-      </div>
-    );
-  }
-}
+// export default class App extends React.Component {
+// eslint-disable-line react/prefer-stateless-function, max-len
+//   render() {
+//     const { children } = this.props; // eslint-disable-line react/prop-types
+//     return (
+//       <div className="layout">
+//         <div className="header">
+//           <h1>HEADER!</h1>
+//           <hr />
+//         </div>
+//         <div className="content">
+//           {children}
+//         </div>
+//         <div className="footer">
+//           <hr />
+//           <h1>FOOTER</h1>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const App = ({ children }) => (
+  <div className="layout">
+    <div className="header">
+      <h1>HEADER!</h1>
+      <hr />
+    </div>
+    <div className="content">
+      {children}
+    </div>
+    <div className="footer">
+      <hr />
+      <h1>FOOTER</h1>
+    </div>
+  </div>
+);
+
+App.propTypes = {
+  children: React.PropTypes.element.isRequired,
+};
+
 //
 // if (__DEVELOPMENT__ && module.hot) {
 //   module.hot.accept('./App.jsx');
 // }
 
-// export default App;
+export default App;
--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 
-class Home extends React.Component { // eslint-disable-line react/prefer-stateless-function, max-len
-  render() {
-    return (
-      <h1>
-        Home page!!.
-      </h1>
-    );
-  }
-}
+const Home = () => (
+  <h1>
+    Home page :) !..!!!!
+  </h1>
+);
 
 export default Home;
